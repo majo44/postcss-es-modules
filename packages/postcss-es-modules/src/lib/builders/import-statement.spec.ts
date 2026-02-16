ab90daf81ee3be344e6dae1e3ea55c50a6baad0b
Wiktor Tuminski <wiktor.tuminski@gmail.com>
2026-02-16 17:39:52 +0100

    feat: update tests and add nvmrc

--- packages/postcss-es-modules/src/lib/builders/import-statement.spec.ts
+++ packages/postcss-es-modules/src/lib/builders/import-statement.spec.ts
@@ -10,40 +10,40 @@ describe('import-statement', () => {
         const builder = fake();
         const node: any = {}
         buildImportInjectStylesStatement(node, builder);
-        expect(builder).calledWith('import { injectStyles } from \'css-es-modules\';\n');
+        expect(builder).calledWith('import { injectStyles } from \'css-es-modules\';',node);
     });
     it('should generate cjs import', () => {
         const builder = fake();
         const node: any = {}
         buildImportInjectStylesStatement(node, builder, { moduleType: 'cjs' });
-        expect(builder).calledWith('const { injectStyles } = require(\'css-es-modules\');\n');
+        expect(builder).calledWith('const { injectStyles } = require(\'css-es-modules\');',node);
     });
 
     it('should generate relative import if eject sibling', () => {
         const builder = fake();
         const node: any = { source: { input: { file: '/a/b/d/a.js'}}}
         buildImportInjectStylesStatement(node, builder, { scriptEjectPath: '/a/b/c', script: 'eject' });
-        expect(builder).calledWith('import { injectStyles } from \'../c/inject-styles\';\n');
+        expect(builder).calledWith('import { injectStyles } from \'../c/inject-styles\';', node);
     });
 
     it('should generate relative import if eject down', () => {
         const builder = fake();
         const node: any = { source: { input: { file: '/a/b/d/a.js'}}}
         buildImportInjectStylesStatement(node, builder, { scriptEjectPath: '/a/b', script: 'eject' });
-        expect(builder).calledWith('import { injectStyles } from \'../inject-styles\';\n');
+        expect(builder).calledWith('import { injectStyles } from \'../inject-styles\';', node);
     });
 
     it('should generate relative import if eject up', () => {
         const builder = fake();
         const node: any = { source: { input: { file: '/a/b/c/a.js'}}}
         buildImportInjectStylesStatement(node, builder, { scriptEjectPath: '/a/b/c/d', script: 'eject' });
-        expect(builder).calledWith('import { injectStyles } from \'./d/inject-styles\';\n');
+        expect(builder).calledWith('import { injectStyles } from \'./d/inject-styles\';', node);
     });
 
     it('should generate relative import if eject to same dir', () => {
         const builder = fake();
         const node: any = { source: { input: { file: '/a/b/c/a.js'}}}
         buildImportInjectStylesStatement(node, builder, { scriptEjectPath: '/a/b/c', script: 'eject' });
-        expect(builder).calledWith('import { injectStyles } from \'./inject-styles\';\n');
+        expect(builder).calledWith('import { injectStyles } from \'./inject-styles\';', node);
     });
 })
